cddd89c1b913a817d4901ec420c172dfd6277796
Manoj Kumar <mankumar46@WKMZTCCC381D.global.publicisgroupe.net>
2022-09-09 15:59:35 +0530

    dynamic handling of Bank Name

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './Header.css';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from '../actions';
 
 const Header = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const userDetails = useSelector(state => {
+        return state.user
+    });
     const clickHandler = () => {
         dispatch(logoutUser());
         navigate('/')
     }
     return (
         <nav className="nav-wrapper">
-            <span className="bank-name">ICICI Bank</span>
+            <span className="bank-name">{userDetails.organisation === 'ICICI' ? 'ICICI Bank' : 'HDFC Bank'}</span>
             <ul className="list-items">
                 <li>Profile</li>
                 <li><a onClick={clickHandler} href="javascript:void(0);">Logout</a></li>
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -8,6 +8,7 @@ const initialState = {
     aadharNumber: '',
     jwtToken: '',
     profileImage: '',
+    organisation: '',
     hasError: false,
     documentsUploaded: false
 }
